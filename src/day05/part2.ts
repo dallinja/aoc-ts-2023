d4cc24b2285cb52cc1a8f3d5fb00d84fb6396e31
Dallin Anderson <dallin@Dallins-MacBook-Pro.local>
2023-12-06 07:32:18 -0700

    Day 5 part 2

--- src/day05/part2.ts
+++ src/day05/part2.ts
@@ -12,12 +12,17 @@ const input = rawInput.reduce<Input>(
     if (line === "") {
       key = "";
     } else if (line.includes("seeds:")) {
-      acc["seeds"] = line.split(": ")[1].split(" ").map(Number);
+      acc["seeds"] = line
+        .split(": ")[1]
+        .split(" ")
+        .map((n) => Math.floor(Number(n)));
     } else if (line.includes("map:")) {
       key = line.split(" map:")[0];
       acc[key] = [];
     } else if (key) {
-      const [destination, source, range] = line.split(" ").map(Number);
+      const [destination, source, range] = line
+        .split(" ")
+        .map((n) => Math.floor(Number(n)));
       acc[key].push({ destination, source, range });
     }
     return acc;
@@ -28,6 +33,8 @@ const input = rawInput.reduce<Input>(
 function solve() {
   // console.log("input", input);
   let minLocation = Infinity;
+  // let winners = {};
+  let seedWinner: number = 0;
   // console.log("seeds: ", seeds);
   for (let i = 0; i < input.seeds.length; i++) {
     if (i % 2 === 1) continue;
@@ -35,6 +42,7 @@ function solve() {
     const range = input.seeds[i + 1];
     for (let j = 0; j < range; j++) {
       let id = seed + j;
+      // const mapIndex: Record<string, number> = {};
       let key = "seed";
       // console.log("seed: ", id);
       let count = 0;
@@ -43,12 +51,16 @@ function solve() {
           (k) => k !== "seeds" && k.startsWith(key)
         );
         if (!map) return;
-        const nextId = input[map].reduce((acc, cat) => {
+        // let winningIndex = 0;
+        const nextId = input[map].reduce((acc, cat, index) => {
           if (id >= cat.source && id < cat.source + cat.range) {
+            // winningIndex = index;
             return cat.destination + (id - cat.source);
           }
           return acc;
         }, id);
+
+        // mapIndex[map] = winningIndex;
 
         key = map.split("-to-")[1];
         id = nextId;
@@ -60,10 +72,15 @@ function solve() {
       }
 
       if (id < minLocation) {
+        // seedWinner = i;
+        seedWinner = seed + j;
+        // winners = { ...mapIndex };
         minLocation = id;
       }
     }
   }
+  console.log("seedWinner", seedWinner);
+  // console.log("winners", winners);
   return minLocation;
 }
 
