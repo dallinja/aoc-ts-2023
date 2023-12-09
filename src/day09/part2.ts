6cd508918a0736f280fdf69f0a351f85be5dad56
Dallin Anderson <dallin@Dallins-MacBook-Pro.local>
2023-12-08 23:10:19 -0700

    Day 9

--- src/day09/part2.ts
+++ src/day09/part2.ts
@@ -1,5 +1,33 @@
-import { parseInput } from '../util';
+import { parseInput } from "../util";
 
-const input = parseInput();
+const rawInput = parseInput({ split: { mapper: false } });
 
-// TODO: Complete Part 2
+const input = rawInput.map((line) =>
+  line.split(" ").map((n) => parseInt(n, 10))
+);
+
+function findNext(nums: number[]): number {
+  const diffs: number[] = [];
+  nums.forEach((num, index) => {
+    if (index === nums.length - 1) return;
+    diffs.push(nums[index + 1] - num);
+  });
+  if (diffs.some((n) => n !== 0)) {
+    const diff = findNext(diffs);
+    return diffs[diffs.length - 1] + diff;
+  }
+  return 0;
+}
+
+function solve() {
+  let sum = 0;
+  input.forEach((nums) => {
+    let nextNum = findNext(nums.reverse());
+    sum += nums[nums.length - 1] + nextNum;
+  });
+  return sum;
+}
+
+const solution = solve();
+
+export default solution;
